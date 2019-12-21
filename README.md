501facce6eb4aa8b57851d9edb49d0360c3e53cf
Riccardo Casatta <riccardo@casatta.it>
2019-12-21 22:03:39 +0100

    use html template

---
+++ README.md
@@ -0,0 +1,19 @@
+# Rusty-paper-wallet
+
+Generates bitcoin paper wallet offline in a single html page with p2wpkh address.
+
+![paper-wallet](rusty-paper-wallet-example.png)
+
+# Usage
+
+Requires [rust](https://www.rust-lang.org/)
+
+```
+$ git clone https://github.com/RCasatta/rusty-paper-wallet
+$ cd rusty-paper-wallet
+$ cargo run
+wif L4rHbEMQJPFn6GUVNZttgfb4HpEZbXeTf3xAWECJHmgqQ2utDhRx
+p2wpkh bc1q6zm2qndz4qtq4h6hh802j3v5gggd4825lntffn
+writing bc1q6zm2qndz4qtq4h6hh802j3v5gggd4825lntffn.html
+
+```
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use bitcoin::util::bip32::ExtendedPrivKey;
+use bitcoin::Address;
 use bitcoin::Network;
+use qrcode::render::svg;
+use qrcode::QrCode;
 use rand::Rng;
 use secp256k1::Secp256k1;
-use bitcoin::Address;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let secp = &Secp256k1::signing_only();
@@ -10,7 +14,34 @@ fn main() {
     let extended_key = ExtendedPrivKey::new_master(Network::Bitcoin, &seed).unwrap();
     let private_key = extended_key.private_key;
     let public_key = private_key.public_key(&secp);
-    let address = Address::p2wpkh(&public_key, Network::Bitcoin);
-    println!("wif {}", private_key.to_wif());
+    let address = Address::p2wpkh(&public_key, Network::Bitcoin).to_string();
+    let wif = private_key.to_wif();
+    println!("wif {}", wif);
     println!("p2wpkh {}", address);
+
+    let wif_qr = QrCode::new(wif.as_bytes()).unwrap();
+    let address_qr = QrCode::new(address.to_uppercase().as_bytes()).unwrap();
+
+    let wif_qr_svg = wif_qr
+        .render()
+        .min_dimensions(200, 200)
+        .dark_color(svg::Color("#000000"))
+        .light_color(svg::Color("#ffffff"))
+        .build();
+    let address_qr_svg = address_qr
+        .render()
+        .min_dimensions(200, 200)
+        .dark_color(svg::Color("#000000"))
+        .light_color(svg::Color("#ffffff"))
+        .build();
+
+    let page = format!(
+        include_str!("template.html"),
+        address, address_qr_svg, wif, wif_qr_svg
+    );
+    let file_name = format!("{}.html", address.to_string());
+
+    println!("writing {}", &file_name);
+    let mut file = File::create(file_name).unwrap();
+    file.write_all(page.as_bytes()).unwrap();
 }
