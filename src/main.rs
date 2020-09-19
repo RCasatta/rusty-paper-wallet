1dd077d3b31ba70b1fd192f4190fc2099fb30cae
Riccardo Casatta <riccardo@casatta.it>
2020-09-19 17:16:18 +0200

    extract fn for repetetive code, use ? instead of unwrap

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,25 @@
-use bitcoin::secp256k1::Secp256k1;
 use bitcoin::util::bip32::ExtendedPrivKey;
-use bitcoin::{Address, Network};
+use bitcoin::{secp256k1, Address, Network};
 use qrcode::render::svg;
 use qrcode::QrCode;
 use rand::Rng;
-use std::fs::File;
-use std::io::prelude::*;
+use std::io::Write;
 
-fn main() {
-    let secp = &Secp256k1::signing_only();
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn main() -> Result<()> {
+    let secp = secp256k1::Secp256k1::signing_only();
     let seed = rand::thread_rng().gen::<[u8; 32]>();
-    let extended_key = ExtendedPrivKey::new_master(Network::Bitcoin, &seed).unwrap();
+    let extended_key = ExtendedPrivKey::new_master(Network::Bitcoin, &seed)?;
     let private_key = extended_key.private_key;
     let public_key = private_key.public_key(&secp);
-    let address = Address::p2wpkh(&public_key, Network::Bitcoin)
-        .unwrap()
-        .to_string();
+    let address = Address::p2wpkh(&public_key, Network::Bitcoin)?.to_string();
     let wif = private_key.to_wif();
     println!("wif {}", wif);
     println!("p2wpkh {}", address);
 
-    let wif_qr = QrCode::new(wif.as_bytes()).unwrap();
-    let address_qr = QrCode::new(address.to_uppercase().as_bytes()).unwrap();
-
-    let wif_qr_svg = wif_qr
-        .render()
-        .min_dimensions(200, 200)
-        .dark_color(svg::Color("#000000"))
-        .light_color(svg::Color("#ffffff"))
-        .build();
-    let address_qr_svg = address_qr
-        .render()
-        .min_dimensions(200, 200)
-        .dark_color(svg::Color("#000000"))
-        .light_color(svg::Color("#ffffff"))
-        .build();
+    let wif_qr_svg = create_svg_qr(&wif)?;
+    let address_qr_svg = create_svg_qr(&address.to_uppercase())?;
 
     let page = format!(
         include_str!("template.html"),
@@ -43,6 +28,18 @@ fn main() {
     let file_name = format!("{}.html", address);
 
     println!("writing {}", &file_name);
-    let mut file = File::create(file_name).unwrap();
-    file.write_all(page.as_bytes()).unwrap();
+    let mut file = std::fs::File::create(file_name)?;
+    file.write_all(page.as_bytes())?;
+
+    Ok(())
+}
+
+fn create_svg_qr(message: &str) -> Result<String> {
+    let qr = QrCode::new(message.as_bytes())?;
+    Ok(qr
+        .render()
+        .min_dimensions(200, 200)
+        .dark_color(svg::Color("#000000"))
+        .light_color(svg::Color("#ffffff"))
+        .build())
 }
