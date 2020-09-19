e292e3ef9e6dcb495f4bfb8b16e415757bed3077
Riccardo Casatta <riccardo@casatta.it>
2020-09-19 15:20:34 +0200

    update deps

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
+use bitcoin::secp256k1::Secp256k1;
 use bitcoin::util::bip32::ExtendedPrivKey;
-use bitcoin::Address;
-use bitcoin::Network;
+use bitcoin::{Address, Network};
 use qrcode::render::svg;
 use qrcode::QrCode;
 use rand::Rng;
-use secp256k1::Secp256k1;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -14,7 +13,9 @@ fn main() {
     let extended_key = ExtendedPrivKey::new_master(Network::Bitcoin, &seed).unwrap();
     let private_key = extended_key.private_key;
     let public_key = private_key.public_key(&secp);
-    let address = Address::p2wpkh(&public_key, Network::Bitcoin).to_string();
+    let address = Address::p2wpkh(&public_key, Network::Bitcoin)
+        .unwrap()
+        .to_string();
     let wif = private_key.to_wif();
     println!("wif {}", wif);
     println!("p2wpkh {}", address);
@@ -39,7 +40,7 @@ fn main() {
         include_str!("template.html"),
         address, address_qr_svg, wif, wif_qr_svg
     );
-    let file_name = format!("{}.html", address.to_string());
+    let file_name = format!("{}.html", address);
 
     println!("writing {}", &file_name);
     let mut file = File::create(file_name).unwrap();
